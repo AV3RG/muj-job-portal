18e68d6e618a7940105644a39b047f7038902e52
RG <goyalrohan2020@gmail.com>
2024-04-07 18:53:47 +0530

    fix: contact page complete

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -1,3 +1,21 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Page() {
-    return <></>
+    return <div className={"m-auto w-full max-w-6xl mt-20 grid grid-cols-2 gap-x-8"}>
+        <div className={"flex flex-col items-center border rounded py-6 px-10 gap-y-4"}>
+            <span className={"text-2xl font-bold"}>Academic</span>
+            <span>
+                Manipal University Jaipur offers a wide range of academic programs in various disciplines. The programs are designed to provide a unique learning experience and are delivered by a team of experienced faculty members. The programs are designed to provide a unique learning experience and are delivered by a team of experienced faculty members.
+            </span>
+            <Link href={"/apply/academic"}><Button className={"mt-4"}>Apply Now</Button></Link>
+        </div>
+        <div className={"flex flex-col items-center border rounded py-6 px-10 gap-y-4"}>
+            <span className={"text-2xl font-bold"}>Non Academic</span>
+            <span>
+                Manipal University Jaipur offers a wide range of academic programs in various disciplines. The programs are designed to provide a unique learning experience and are delivered by a team of experienced faculty members. The programs are designed to provide a unique learning experience and are delivered by a team of experienced faculty members.
+            </span>
+            <Link href={"/apply/non-academic"}><Button className={"mt-4"}>Apply Now</Button></Link>
+        </div>
+    </div>
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -11,22 +11,24 @@ import data from "@/constants/contacts.json"
 
 export default async function ContactPage() {
 
+    const dataTyped = data as Array<{Office: string, Extension: number[], "Email ID": string[]}>
+
     return <div className={"flex"}>
         <div className={"w-full flex flex-col items-center justify-center mt-10"}>
             <div className={"text-4xl font-semibold"}>Contact Us</div>
             <div>
-                <Table>
+                <Table >
                     <TableCaption>Important MUJ Contacts</TableCaption>
                     <TableHeader>
-                        <TableRow>
+                        <TableRow className={"text-xl"}>
                             <TableHead>Office</TableHead>
                             <TableHead>Phone</TableHead>
                             <TableHead>Extension</TableHead>
                             <TableHead>Email ID</TableHead>
                         </TableRow>
                     </TableHeader>
-                    <TableBody>
-                        {data.map((row, index) => {
+                    <TableBody className={"text-lg"}>
+                        {dataTyped.map((row, index) => {
                             return <TableRow key={index}>
                                 <TableCell>{row["Office"]}</TableCell>
                                 {index == 0 && <TableCell rowSpan={13}>0141 - 3999100</TableCell>}
